fa6f0c682bf0136ddf6e14cac355041e6829e734
osk2 <sc60714@gmail.com>
2017-08-05 18:05:23 +0800

    Add debugging mode

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 Vue.prototype.$http = axios;
 
-const TYPHOON_INFO_API = 'https://api.wmega.tw/wmega/webapi/typh/getTyphoonInfo';
+const DEBUG_MODE = false;
+const TYPHOON_INFO_API = DEBUG_MODE ? '/fake-data.json' : 'https://api.wmega.tw/wmega/webapi/typh/getTyphoonInfo';
 const CORNER_COORDS = {
   topLeft: [101.074, 43.97638],
   downLeft: [101.074, 8.5433],
